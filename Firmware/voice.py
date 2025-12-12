1b4271798077cefbbdfcd151507ed347a4b73c58
gethin101 <gethingrice@gmail.com>
2025-12-12 07:48:54 +0000

    Create voice.py

---
+++ Firmware/voice.py
@@ -0,0 +1,13 @@
+#simple template for my voice recog
+
+import time
+
+class VoiceModule:
+    def __init__(self):
+        print("Ready")
+
+    #will change this later
+    def listen(self):
+        time.sleep(0.05)
+
+        return None
